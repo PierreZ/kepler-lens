54de6bda7f7658177c1b42a0016620fc74401c1f
Pierre Zemb <contact@pierrezemb.fr>
2017-09-03 22:36:51 +0200

    feat: convert csv

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import tarfile
 import click
 import kplr
 from string import Template
+from pyke import kepconvert
 import json
 import sys
 
@@ -58,7 +59,7 @@ MARK
     DUP NAME RENAME
     $$data 'attributes' GET SETATTRIBUTES
 %> FOREACH
-COUNTTOMARK ->LIST '$wt' UPDATE
+COUNTTOMARK ->LIST '$wt' META
 """
 
 
@@ -67,19 +68,17 @@ def cli():
     pass
 
 @cli.command()
-@click.option('--token', default='', help='warp WRITE token')
-@click.option('--endpoint', default='http://localhost:8080', help='warp endpoint')
 @click.option('--path', default='/tmp/kepler', help='kepler download folder')
 @click.option('--limit', default='all', help='comma separated list of compagne to download.')
 @click.argument('dataset', type=click.Choice(['k2', 'kepler']))
-def init(token, endpoint, path, limit, dataset):
+def init(path, limit, dataset):
     if dataset == "kepler":
-        download_campagne(token, endpoint, path, limit, "kepler", KEPLER_TARFILES)
+        download_campagne(path, limit, "kepler", KEPLER_TARFILES)
     if dataset == "k2":
-        download_campagne(token, endpoint, path, limit, "k2", K2_TARFILES)
+        download_campagne(path, limit, "k2", K2_TARFILES)
 
 
-def download_campagne(token, endpoint, path, limit, dataset, dictFiles):
+def download_campagne(path, limit, dataset, dictFiles):
     click.echo('Initializing the database, downloading {} dataset...'.format(dataset))
 
     baseurl = ARCHIVE_URL.format(dataset)
@@ -103,51 +102,70 @@ def download_campagne(token, endpoint, path, limit, dataset, dictFiles):
 
     for compagne, nbrfiles in compagnes.items():
         click.echo('downloading {} dataset, {} files'.format(compagne, nbrfiles))
-
         for nbrfile in range(1, nbrfiles + 1):
-
-            filename = 'public_{}_long_{}'.format(compagne, nbrfile)
-            outfolder = lightcurvesfolder + filename
-
-             # checking if already downloaded
-            if os.path.exists(outfolder):
-                click.echo('folder {} already exists'.format(outfolder))
-                click.echo('{} already downloaded, moving on'.format(filename))
-                continue
-            else:
-                os.makedirs(outfolder)
-
-
-            url = baseurl + compagne
-            if dataset == "kepler":
-                url = url + "_public"
-            url = url + "/" + filename + ".tgz"
-
-            click.echo('downloading {}'.format(url))
-            outfile = lightcurvesfolder+filename+".tgz"
-            wget.download(url, out=outfile)
-
-            click.echo('{} downloaded, untar in progress'.format(filename))
-
-            tar = tarfile.open(outfile)
-            tar.extractall(path=outfolder)
-            tar.close()
-
-            click.echo('untar of {} done'.format(filename))
-            os.remove(outfile)
-            click.echo('removing archive {} done'.format(outfile))
-
+            dl_campagne(compagne, nbrfile, lightcurvesfolder, baseurl, dataset)
+            generate_csv(compagne, nbrfile, lightcurvesfolder, csvfolder)
 
         click.echo('compagne {} done!'.format(compagne))
 
-    click.echo('all compagnes are fetched, generating csv')
+    click.echo('all compagnes are fetched, bye')
+
+def generate_csv(compagne, nbrfile, lightcurvesfolder, csvfolder):
+
+    filename = 'public_{}_long_{}/'.format(compagne, nbrfile)
+    lightcurvesfolder = lightcurvesfolder + filename
+    directory = os.fsencode(lightcurvesfolder)
+
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename.endswith(".fits") and not os.path.exists(csvfolder + filename.replace(".fits", ".csv")):
+            # Generating CSV 
+            kepconvert(lightcurvesfolder + filename, "fits2csv", "TIME,SAP_FLUX,SAP_FLUX_ERR,SAP_QUALITY",
+                       outfile=csvfolder + filename.replace(".fits", ".csv"),
+                       baddata=False, overwrite=False, verbose=False)
+            # print(os.path.join(directory, filename))
+            continue
+        else:
+            continue
+
+
+def dl_campagne(compagne, nbrfile, lightcurvesfolder, baseurl, dataset):
+
+    filename = 'public_{}_long_{}'.format(compagne, nbrfile)
+    outfolder = lightcurvesfolder + filename
+    # checking if already downloaded
+    if os.path.exists(outfolder):
+        click.echo('folder {} already exists'.format(outfolder))
+        click.echo('{} already downloaded, moving on'.format(filename))
+        return
+
+    os.makedirs(outfolder)
+    url = baseurl + compagne
+    if dataset == "kepler":
+        url = url + "_public"
+    url = url + "/" + filename + ".tgz"
+
+    click.echo('downloading {}'.format(url))
+    outfile = lightcurvesfolder+filename+".tgz"
+    wget.download(url, out=outfile)
+
+    click.echo('{} downloaded, untar in progress'.format(filename))
+
+    tar = tarfile.open(outfile)
+    tar.extractall(path=outfolder)
+    tar.close()
+
+    click.echo('untar of {} done'.format(filename))
+    os.remove(outfile)
+    click.echo('removing archive {} done'.format(outfile))
+
 
 @cli.command()
 @click.option('--wtoken', default='w', help='warp WRITE token')
 @click.option('--rtoken', default='r', help='warp READ token')
 @click.option('--endpoint', default='http://localhost:8080', help='warp endpoint')
 @click.option('--limit', default=0, help='limit on koi_period')
-def update(wtoken, rtoken, endpoint, limit):
+def update(wtoken, limit):
 
     client = kplr.API()
 
